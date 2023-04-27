9d7270696e6ec3e807961e23286f1ded3caa458f
rozan <np03cs4a210016@heraldcollege.edu.np>
2023-04-27 16:58:14 +0545

    new updates pushed..git add .

--- ForkAndKnife/urls.py
+++ ForkAndKnife/urls.py
@@ -13,7 +13,14 @@ urlpatterns = [
     path("about/", views.about, name="aboutPage"),
    # path("profile/<str:username>/", views.profile, name="profilePage"),
    path("profile/", views.profile, name="profilePage"),
-   path('menulist', views.menuList, name="menulistPage"),
+   path('food', views.menuFoodList, name="menuFoodPage"),
+     path('drink', views.menuDrinkList, name="menuDrinkPage"),
+      path('bakery', views.menuBakeryList, name="menuBakeryPage"),
+   path('desert', views.menuDesertList, name="menuDesertPage"),
+   #   path('menulist', views.menuList, name="menulistPage"),
+   path('orderitem/<int:id>/', views.orderItem, name="orderPage",),
+
+
    ##password reset
  #  path('password_reset/', auth_views.PasswordResetView.as_view(template_name ="ForkAndKnife/forgotpass.html"),
   #       name='password_reset'),
--- ForkAndKnife/views.py
+++ ForkAndKnife/views.py
@@ -10,8 +10,16 @@ from .forms import CustomUserCreationForm
 from .models import Menu, SubCategory
 # Create your views here.
 
+# def index(request:
+    # menus = Menu.objects.all)
+    # return render(request, "ForkAndKnife/index.html", {'menuss': menus)
+
+# Define a view function to display the home page
 def index(request):
+    # Get all the Menu objects from the database
     menus = Menu.objects.all()
+    
+    # Render the index.html template with the menus as context
     return render(request, "ForkAndKnife/index.html", {'menuss': menus})
 
 def signin(request):
@@ -32,33 +40,62 @@ def signin(request):
      #   return render(request, 'ForkAndKnife/signin.html')
     return render(request, "ForkAndKnife/signin.html")
 
+# def signup(request):
+    # if request.method == 'POST':
+        # form = CustomUserCreationForm(request.POST)
+        # if form.is_valid():
+            # form.save()
+            # return redirect('loginPage')
+    # else:
+        # form = CustomUserCreationForm()
+    # return render(request, 'ForkAndKnife/joinnow.html', {'form': form})
+# 
+    #return render(request, "ForkAndKnife/joinnow.html")
+
+# Define a view function to handle user registration
 def signup(request):
+    
+    # If the request method is POST, process the form data
     if request.method == 'POST':
+        # Create a new instance of the CustomUserCreationForm using the form data
         form = CustomUserCreationForm(request.POST)
+        
+        # Check if the form is valid
         if form.is_valid():
+            # Save the user to the database
             form.save()
+            
+            # Redirect to the login page
             return redirect('loginPage')
+    
+    # If the request method is not POST, display an empty form
     else:
         form = CustomUserCreationForm()
+    
+    # Render the joinnow.html template with the form
     return render(request, 'ForkAndKnife/joinnow.html', {'form': form})
 
-    #return render(request, "ForkAndKnife/joinnow.html")
 
 def logoutview(request):
     logout(request)
     return redirect('indexPage')
 
+'''
 #@login_required
 def home(request):
     if request.user.is_authenticated:
         menus = Menu.objects.all()
         return render(request, 'ForkAndKnife/home.html',{'menuss': menus})
     else:
-        return redirect('loginPage')
+        return redirect('loginPage') '''
+
+def home(request):
+    menus = Menu.objects.all()
+    return render(request, 'ForkAndKnife/home.html',{'menuss': menus})
 
 
 def menu(request):
-    return render(request, "ForkAndKnife/menu.html")
+    return render(request, "ForkAndKnife/menu.html",)
 
 def about(request):
     return render(request, 'ForkAndKnife/about.html')
@@ -73,8 +110,40 @@ def profile(request):
     return render(request, 'ForkAndKnife/profile.html')
 
 
-def menuList(request):
+def menuFoodList(request):
     obj = SubCategory.objects.all()
     menus = Menu.objects.all()
 
-    return render(request, "ForkAndKnife/menulist.html", {'objj': obj , 'menuss': menus})
+    return render(request, "ForkAndKnife/menuFood.html", {'objj': obj , 'menuss': menus})
+
+def orderItem(request, id):
+    #menus = Menu.objects.all()
+    order = get_object_or_404(Menu, id = id)
+    #order = Menu.objects.get(id=id)
+    return render(request, "ForkAndKnife/orderMenu.html", { 'order': order})
+
+# def orderItem(request):
+    # return render(request, "ForkAndKnife/orderMenu.html")
+# 
+def menuList(request):
+    return HttpResponse("hello")
+
+def menuDrinkList(request):
+     obj = SubCategory.objects.all()
+     menus = Menu.objects.all()
+    
+     return render(request, "ForkAndKnife/menuDrink.html", {'objj': obj , 'menuss': menus})
+
+
+def menuBakeryList(request):
+     obj = SubCategory.objects.all()
+     menus = Menu.objects.all()
+    
+     return render(request, "ForkAndKnife/menuBakery.html", {'objj': obj , 'menuss': menus})
+
+
+def menuDesertList(request):
+     obj = SubCategory.objects.all()
+     menus = Menu.objects.all()
+    
+     return render(request, "ForkAndKnife/menuDeserts.html", {'objj': obj , 'menuss': menus})
